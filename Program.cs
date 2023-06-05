c3475a261da883b8cc0990c4e57e1ddc194ba203
Alexandra <alexadudki11@gmail.com>
2023-06-05 12:38:54 +0300

    hx

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 int mes(string message){
     System.Console.Write(message);
-    string value = Console.ReadLine();
+    string value = Console.ReadLine()!;
     int result = Convert.ToInt32(value);
     return result;
 }
